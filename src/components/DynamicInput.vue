53cd3dd6444bbc138d235149e5749bd6086292fa
이승로 <iseungro@iseungloui-MacBookPro.local>
2022-01-04 23:31:32 +0900

    [22.01.04:11:31][이승로] 메인작업

---
+++ src/components/DynamicInput.vue
@@ -0,0 +1,190 @@
+<template>
+  <div class="g-input relative flex flex-col" :class="errored ? 'errored' : ''">
+    <div class="flex rounded-xl bg-white">
+      <div
+        class="relative flex-grow rounded-xl transition duration-500"
+        :class="[
+          inputDynamicStyles,
+          isSmall ? 'px-6 py-6' : 'px-10 py-6',
+          borderless ? '' : 'border',
+        ]"
+      >
+        <div
+          class="
+            placeholder
+            absolute
+            top-6
+            origin-left
+            transition-transform
+            duration-500
+            pointer-events-none
+            whitespace-nowrap
+            z-10
+          "
+          :class="[
+            isFocused || valueBind.length > 0
+              ? 'transform -translate-y-4 scale-75'
+              : '',
+            lastCharEmphasized ? 'emphasized' : '',
+          ]"
+          v-html="placeholder"
+        ></div>
+        <input
+          v-if="!isTextArea"
+          class="
+            w-full
+            font-semibold
+            outline-none
+            transition-transform
+            duration-500
+          "
+          :class="
+            isFocused || valueBind.length > 0
+              ? 'transform translate-y-[10px]'
+              : ''
+          "
+          v-model="valueBind"
+          @focus="toggleFocus(true)"
+          @blur="toggleFocus(false)"
+        />
+        <textarea
+          v-if="isTextArea"
+          class="
+            w-full
+            font-semibold
+            outline-none
+            transition-transform
+            duration-500
+            resize-none
+            leading-5
+          "
+          :class="
+            isFocused || valueBind.length > 0
+              ? 'transform translate-y-[10px]'
+              : ''
+          "
+          style="min-height: 5rem"
+          v-model="valueBind"
+          @focus="toggleFocus(true)"
+          @blur="toggleFocus(false)"
+          @keydown="resizeTextarea($event)"
+        />
+      </div>
+    </div>
+
+    <div
+      v-if="errored"
+      class="error-label top-full mt-2 text-sm"
+      :class="isSmall ? 'ml-6' : 'ml-10'"
+    >
+      {{ errorLabel }}
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref, toRefs, computed, watch } from "vue";
+
+export default {
+  props: {
+    placeholder: {
+      type: String,
+      default: "Placeholder",
+    },
+    errored: {
+      type: Boolean,
+      default: false,
+    },
+    errorLabel: String,
+    disabled: Boolean,
+    isSmall: Boolean,
+    value: String,
+    validator: {
+      type: Function,
+      default: () => true,
+    },
+    isTextArea: {
+      type: Boolean,
+      default: false,
+    },
+    lastCharEmphasized: {
+      type: Boolean,
+      default: false,
+    },
+    borderless: Boolean,
+    focused: Boolean,
+  },
+  setup(props, { emit }) {
+    const { disabled, errored, value, validator } = toRefs(props);
+
+    const isFocused = ref(false);
+    const isErrored = ref(false);
+    const valueBind = ref(value.value);
+    const inputDynamicStyles = computed(() => {
+      let border = isFocused.value ? "border-primary" : "border-gray-300";
+      border = errored.value ? "border-red-600" : border;
+
+      let disabledStyle = disabled.value
+        ? " bg-gray-300 text-g-gray pointer-events-none"
+        : "";
+
+      return border + disabledStyle;
+    });
+
+    const toggleFocus = (value) => {
+      isFocused.value = value;
+
+      emit("update:focused", isFocused.value);
+    };
+
+    const resizeTextarea = (e) => {
+      console.log(e.target);
+
+      e.target.style.height = e.target.scrollHeight + "px";
+    };
+
+    watch(valueBind, (to) => {
+      let result = validator.value(to);
+
+      emit("update:errored", !result.result);
+
+      console.log(result);
+      if (result.fixed || result.fixed === "") {
+        valueBind.value = result.fixed;
+      } else {
+        emit("update:value", to);
+      }
+    });
+
+    watch(value, (to) => {
+      valueBind.value = to;
+    });
+
+    return {
+      isErrored,
+      isFocused,
+      valueBind,
+      inputDynamicStyles,
+      toggleFocus,
+      resizeTextarea,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+.g-input {
+  & .placeholder {
+    @apply text-gray-500;
+  }
+  & .emphasized::after {
+    content: "*";
+    color: red;
+  }
+
+  &.errored,
+  &.errored .placeholder {
+    @apply text-red-600;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,8 +4,22 @@
     <section class="main-carousel mb-2">
       <img class="w-full object-cover" :src="currentCarousel" />
     </section>
-    <section class="container mx-auto flex">
-      <div class="bg-primary text-white font-bold text-2xl px-8">CS Notice</div>
+    <section class="container mx-auto mb-2 flex">
+      <div
+        class="
+          flex
+          items-center
+          bg-primary
+          text-white
+          font-bold
+          text-2xl
+          px-8
+          whitespace-nowrap
+          flex-shrink-0
+        "
+      >
+        CS Notice
+      </div>
       <div class="flex-grow w-full flex bg-primary p-1">
         <div class="flex flex-col w-1/2 bg-white py-4 mr-1">
           <p
@@ -54,8 +68,13 @@
         </div>
       </div>
     </section>
-    <section class="slider-section container mx-auto mt-2">
-      <carousel :items-to-show="2.5" class="w-full text-left">
+    <section class="slider-section container mx-auto mb-2">
+      <carousel
+        :items-to-show="2.5"
+        :wrap-around="true"
+        :snap-align="'start'"
+        class="w-full text-left"
+      >
         <slide v-for="(e, i) in events" :key="i" class="pr-2">
           <div class="flex flex-col px-4 py-4 bg-gray-200 rounded-xl">
             <p
@@ -84,19 +103,83 @@
             </div>
           </div>
         </slide>
+        <template #addons>
+          <navigation />
+        </template>
       </carousel>
+    </section>
+    <section class="w-full bg-gray-200 mb-2">
+      <div class="container mx-auto py-8">
+        <p class="text-4xl font-bold mb-8">익명 건의사항</p>
+        <div class="flex flex-col">
+          <div class="flex w-full mb-4">
+            <dynamic-input
+              class="flex-grow mr-4"
+              :placeholder="'제목'"
+              v-model:errored="nameInput.errored"
+              :errorLabel="nameInput.errorLabel"
+              v-model:value="nameInput.value"
+              :validator="nameInput.validator"
+            />
+            <dynamic-input
+              :placeholder="'학년'"
+              :isSmall="true"
+              v-model:errored="yearInput.errored"
+              :errorLabel="yearInput.errorLabel"
+              v-model:value="yearInput.value"
+              :validator="yearInput.validator"
+            />
+          </div>
+          <div class="flex flex-col bg-white rounded-xl">
+            <dynamic-input
+              :placeholder="'내용'"
+              :isTextArea="true"
+              v-model:errored="contentInput.errored"
+              :errorLabel="contentInput.errorLabel"
+              v-model:value="contentInput.value"
+              :validator="contentInput.validator"
+              :borderless="true"
+              v-model:focused="contentInput.focused"
+            />
+            <button
+              class="
+                self-end
+                mr-4
+                mb-4
+                px-4
+                py-2
+                bg-primary
+                text-white
+                rounded-lg
+              "
+            >
+              건의하기
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="w-full bg-gray-200">
+      <div class="container mx-auto py-8">
+        <p>[55069] 전라북도 전주시 완산구 천잠로 303 공학1관 208호</p>
+        <p class="mb-16">
+          공과대학 행정실 | TEL: 063-22-2372 | FAX: 063-220-2056
+        </p>
+        <p>Copyrights (C) 2022 전주대학교 컴퓨터공학과</p>
+      </div>
     </section>
   </div>
 </template>
 
 <script>
 import { computed, onMounted, ref } from "vue";
-import { Carousel, Slide } from "vue3-carousel";
+import { Carousel, Slide, Navigation, Pagination } from "vue3-carousel";
 
 import "vue3-carousel/dist/carousel.css";
 
 import MainHeader from "../components/MainHeader.vue";
 import TextBox from "../components/TextBox.vue";
+import DynamicInput from "../components/DynamicInput.vue";
 
 export default {
   name: "Home",
@@ -104,7 +187,10 @@ export default {
     MainHeader,
     Carousel,
     Slide,
+    Navigation,
+    Pagination,
     TextBox,
+    DynamicInput,
   },
   setup() {
     const carouselCurrentIdx = ref(0);
@@ -202,6 +288,54 @@ export default {
         thumbnail: "https://via.placeholder.com/1000x1000",
         datetime: "2021-01-01",
       },
+      {
+        idx: "03",
+        title: "이벤트 3",
+        content: `
+          대통령의 임기는 5년으로 하며, 중임할 수 없다.
+          교육의 자주성·전문성·정치적 중립성 및 대학의 자율성은
+          법률이 정하는 바에 의하여 보장된다.
+          대법원장은 국회의 동의를 얻어 대통령이 임명한다.
+          모든 국민은 그 보호하는 자녀에게 적어도
+          초등교육과 법률이 정하는 교육을 받게 할 의무를 진다.
+          언론·출판에 대한 허가나 검열과
+          집회·결사에 대한 허가는 인정되지 아니한다.
+        `,
+        thumbnail: "https://via.placeholder.com/1000x1000",
+        datetime: "2021-01-01",
+      },
+      {
+        idx: "03",
+        title: "이벤트 4",
+        content: `
+          대통령의 임기는 5년으로 하며, 중임할 수 없다.
+          교육의 자주성·전문성·정치적 중립성 및 대학의 자율성은
+          법률이 정하는 바에 의하여 보장된다.
+          대법원장은 국회의 동의를 얻어 대통령이 임명한다.
+          모든 국민은 그 보호하는 자녀에게 적어도
+          초등교육과 법률이 정하는 교육을 받게 할 의무를 진다.
+          언론·출판에 대한 허가나 검열과
+          집회·결사에 대한 허가는 인정되지 아니한다.
+        `,
+        thumbnail: "https://via.placeholder.com/1000x1000",
+        datetime: "2021-01-01",
+      },
+      {
+        idx: "03",
+        title: "이벤트 5",
+        content: `
+          대통령의 임기는 5년으로 하며, 중임할 수 없다.
+          교육의 자주성·전문성·정치적 중립성 및 대학의 자율성은
+          법률이 정하는 바에 의하여 보장된다.
+          대법원장은 국회의 동의를 얻어 대통령이 임명한다.
+          모든 국민은 그 보호하는 자녀에게 적어도
+          초등교육과 법률이 정하는 교육을 받게 할 의무를 진다.
+          언론·출판에 대한 허가나 검열과
+          집회·결사에 대한 허가는 인정되지 아니한다.
+        `,
+        thumbnail: "https://via.placeholder.com/1000x1000",
+        datetime: "2021-01-01",
+      },
     ]);
 
     onMounted(() => {
@@ -215,11 +349,77 @@ export default {
       }, 2500);
     });
 
+    const nameInput = ref({
+      errored: false,
+      errorLabel: "",
+      value: "",
+      validator: (data) => {
+        if (data.length === 0) {
+          nameInput.value.errorLabel = "제목을 입력해주세요!";
+          return {
+            result: false,
+          };
+        }
+
+        return {
+          result: true,
+        };
+      },
+    });
+
+    const yearInput = ref({
+      errored: false,
+      errorLabel: "",
+      value: "",
+      validator: (data) => {
+        if (isNaN(data)) {
+          console.log("not num");
+          return {
+            result: false,
+            fixed: "",
+          };
+        }
+
+        if (parseInt(data) < 0 || parseInt(data) > 4) {
+          yearInput.value.errorLabel = "학년은 1~4 사이로 입력해주세요!";
+          return {
+            result: false,
+          };
+        }
+
+        return {
+          result: true,
+        };
+      },
+    });
+
+    const contentInput = ref({
+      errored: false,
+      errorLabel: "",
+      value: "",
+      validator: (data) => {
+        if (data.length === 0) {
+          contentInput.value.errorLabel = "내용을 채워주세요!";
+          return {
+            result: false,
+          };
+        }
+
+        return {
+          result: true,
+        };
+      },
+      focused: false,
+    });
+
     return {
       carouselImages,
       currentCarousel,
       notices,
       events,
+      nameInput,
+      yearInput,
+      contentInput,
     };
   },
 };
